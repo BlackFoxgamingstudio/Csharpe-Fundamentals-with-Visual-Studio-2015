d8b29e0aee1b0a3ff2d25df8461e924533b1a15f
naruto <blackfoxgamingstudio@gmail.com>
2017-04-19 09:00:57 -0700

    Create user inputs name.cs
    user inputs name and program says hello

---
+++ user inputs name.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("hello " + name);
+            Console.ReadLine();
+        }
+    }
+}
